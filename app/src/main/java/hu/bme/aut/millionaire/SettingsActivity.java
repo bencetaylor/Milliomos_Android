a6e8da7c1038eaecf337468cf7d884a31583f2ee
DESKTOP-NJV7KVA\Bence <bencetaylor@gmail.com>
2017-11-27 16:40:29 +0100

    Added setting- and scoreboardActivity back button onClickListener

--- app/src/main/java/hu/bme/aut/millionaire/SettingsActivity.java
+++ app/src/main/java/hu/bme/aut/millionaire/SettingsActivity.java
@@ -1,13 +1,28 @@
 package hu.bme.aut.millionaire;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private Button btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        btn_back = (Button) findViewById(R.id.btn_settings_back);
+
+        final Intent intent = new Intent(this, MenuActivity.class);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                startActivity(intent);
+            }
+        });
     }
 }
