ceb3e034e8ba77c994a43db75dfc6fab33d5133e
DESKTOP-1DUUI9S\Bence <bencetaylor@gmail.com>
2017-12-05 16:09:32 +0100

    Valami nem fasza

--- app/src/main/java/hu/bme/aut/millionaire/ScoreboardActivity.java
+++ app/src/main/java/hu/bme/aut/millionaire/ScoreboardActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
 import android.widget.Button;
 
 import java.util.List;
@@ -24,7 +23,7 @@ public class ScoreboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scoreboard);
-        
+
         /*
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -42,13 +41,13 @@ public class ScoreboardActivity extends AppCompatActivity {
 
         final Intent intent = new Intent(this, MenuActivity.class);
 
-        back = (Button) findViewById(R.id.btn_scoreboard_back);
+        /*back = (Button) findViewById(R.id.btn_scoreboard_back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view){
                 startActivity(intent);
             }
-        });
+        });*/
     }
 
     private void initRecyclerView() {
